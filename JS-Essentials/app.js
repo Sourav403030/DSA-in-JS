095dd937edd8357d301c8b0848732c1a725a1e88
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-20 23:08:22 +0530

    DSA Chapter 1 Done

---
+++ JS-Essentials/app.js
@@ -0,0 +1,86 @@
+//-------------------QUESTION 1-------------------//
+// Sum of two Integers.
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+
+//-------------------QUESTION 2-------------------//
+// Sum and Message
+
+// let a = 10;
+// let b = 20;
+// console.log(`The sum of ${a} & ${b} is ${a+b}`);
+
+//-------------------QUESTION 3-------------------//
+// Accept input from the user and print the answer
+
+// let prompt = require("prompt-sync")();
+
+// let age = prompt("Enter the age");
+// console.log(age);
+
+//-------------------QUESTION 4-------------------//
+// Swap two variables using 3 variable method.
+
+// let a = 10;
+// let b = 20;
+
+// console.log(`Before swapping: ${a}, ${b}`);
+
+// let temp = a;
+// a = b;
+// b = temp;
+
+// console.log(`After swapping: ${a}, ${b}`);
+
+//-------------------QUESTION 5-------------------//
+// Swap two variables using 2 variable method.
+
+// let a = 10;
+// let b = 20;
+
+// console.log(`Before swapping: ${a}, ${b}`);
+
+// a = a + b 
+// b = a - b; 
+// a = a - b; 
+
+// console.log(`After swapping: ${a}, ${b}`);
+
+//-------------------QUESTION 6-------------------//
+// Swap two variables using destructuring method.
+
+// let a = 10;
+// let b = 20;
+
+// console.log(`Before swapping: ${a}, ${b}`);
+
+// [a,b] = [b,a];
+
+// console.log(`After swapping: ${a}, ${b}`);
+
+//-------------------QUESTION 7-------------------//
+// Compound Interest Calculation.
+
+// Accept the Principal Amount(P), Annual Interest(r), the number of years(t) and the number of times the interest is compounded per year(n). 
+// Write a program to calculate the Compound Interest. The formula to calculate Compound Interest is - 
+
+// A = P * (1 + r/n)**(n*t)
+
+// the Compund Interest is then calculated as CI = A - P.
+
+
+// function calculateCompoundInterest(P, r, t, n) {
+//     let A = P * (1 + r/n)**(n*t);
+    
+//     let CI = A - P;
+
+//     CI = CI.toFixed(2);
+    
+//     console.log(CI);
+// }
+
+// calculateCompoundInterest(1000, 0.5, 10, 4);
+
