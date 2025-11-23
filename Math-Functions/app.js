bcd6480f27ca5c5025a6b54111f586eb31e5eaa1
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-23 10:36:31 +0530

    3 questions done

---
+++ Math-Functions/app.js
@@ -0,0 +1,33 @@
+//-------------------QUESTION 1-------------------//
+// Generate a 4-digit OTP.
+
+// let otp = (Math.random()*9000)+1000;
+// otp = Math.floor(otp);
+
+// console.log(`Your OTP is: ${otp}`);
+
+//-------------------QUESTION 2-------------------//
+// Area of a Triangle by Heron's formula.
+
+// let prompt = require("prompt-sync")()
+
+// let a = +prompt("Enter the first side: ");
+// let b = +prompt("Enter the second side: ");
+// let c = +prompt("Enter the third side: ");
+
+// let s = (a + b + c) / 2; 
+
+// let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+
+// console.log(`The area of the triangle is ${area}`);
+
+//-------------------QUESTION 3-------------------//
+// Circumference of a Circle
+
+// let prompt = require("prompt-sync")()
+
+// let r = +prompt("Enter the radius: ");
+
+// let cir = 2 * Math.PI * r;
+
+// console.log(`The circumference of the circle is: ${cir}`);
